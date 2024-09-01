f3fa5948dfea7a8ae839f15deed790b97e424757
Shubham Sawant <sawshubh11@gmail.com>
2024-09-01 10:39:25 +0530

    added prod configuration

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,13 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8b5vd=g@ydy&yl6$2!c@n2)oxs(4!@=xn47g3werp33n6dtqrg"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
